3588904ebdc3ea8b638157add032f07cec7f8750
Sam S <salinas144@gmail.com>
2019-08-16 18:36:04 -0400

    Add full functionality to copy
    Note:
    Still need to adjust Loading; going to make it a new component.

--- src/Pages/Copy.jsx
+++ src/Pages/Copy.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { StateContext } from "State";
 import { setToken } from "Modules/token";
 import { getHashFragment, hideHashFragment } from "Helpers/hash";
@@ -24,10 +23,12 @@ export default class Copy extends React.Component {
     super(props);
 
     this.state = {
+      error: false,
       currentUser: "",
       playlistToCopy: "",
       isCopying: false,
-      copyCompleted: true
+      copyCompleted: false,
+      newPlaylistUrl: ""
     };
   }
 
@@ -38,92 +39,113 @@ export default class Copy extends React.Component {
 
     hideHashFragment();
 
-    if (!hash) return;
+    if (!hash) {
+      this.setState({
+        error: true
+      });
+      return;
+    }
 
     let token = hash.access_token;
 
-    if (token) {
-      // const [ { token }, dispatch ] = this.context;
-      let dispatch = this.context[1];
-
-      let userData = await getCurrentUserProfile(token);
-      // console.log(userData);
+    if (!token) {
       this.setState({
-        currentUser: userData
+        error: true
       });
-
-      dispatch(setToken(token));
     }
+
+    // const [ { token }, dispatch ] = this.context;
+    let dispatch = this.context[1];
+
+    let userData = await getCurrentUserProfile(token);
+    // console.log(userData);
+    this.setState({
+      currentUser: userData
+    });
+
+    dispatch(setToken(token));
   };
 
   onFormSubmit = async e => {
     e.preventDefault();
 
-    let { currentUser, playlistToCopy } = this.state;
+    setTimeout(async () => {
+      let { currentUser, playlistToCopy } = this.state;
 
-    let playlistToCopyArr = playlistToCopy.split("/").filter(Boolean);
+      let playlistToCopyArr = playlistToCopy.split("/").filter(Boolean);
 
-    if (playlistToCopyArr.length === 0) {
-      alert(
-        "Your 'Playlist to copy' link isn't in the right format. Should look like this: " +
-          placeholder +
-          ""
-      );
-      return;
-    }
-
-    playlistToCopy = playlistToCopyArr.pop();
-
-    const [{ token }] = this.context;
-
-    let tracks = [];
-    let data = undefined;
-    let next = undefined;
-    let index = 0;
-
-    let playlistInfo = await getPlaylistInfo(token, playlistToCopy);
-    console.log("Playlist Info", playlistInfo);
-
-    // Create new playlist with the same name
-    let newPlaylist = await createPlaylist(
-      token,
-      currentUser.id,
-      playlistInfo.name,
-      playlistInfo.description
-    );
-    console.log("New playlist", newPlaylist);
-
-    // Get playlist tracks only allow 100 at a time.
-    // The spotify api kindly provides a 'next' property with a url
-    // to get the next 100 tracks.
-    do {
-      data = await getPlaylistTracks(token, playlistToCopy, next);
-      next = data.next;
-      // tracks = tracks.concat(data.items);
-      tracks[index] = data.items;
-      index++;
-    } while (next);
-
-    console.log(tracks);
-
-    // 'Add tracks to playlists' only allows 100 tracks at a time
-    for (let i = 0; i < tracks.length; i++) {
-      let tracklist = tracks[i].map(item => {
-        return item.track.uri;
-      });
-
-      let returnedData = await addTracksToPlaylist(
-        token,
-        newPlaylist.id,
-        tracklist
-      );
-
-      if (returnedData.error) {
-        console.log("Something went wrong!", returnedData);
-        break;
+      if (playlistToCopyArr.length === 0) {
+        alert(
+          "Your 'Playlist to copy' link isn't in the right format. Should look like this: " +
+            placeholder +
+            ""
+        );
+        return;
       }
-      console.log(`Added track set ${i + 1} of ${tracks.length}`);
-    }
+
+      playlistToCopy = playlistToCopyArr.pop();
+
+      const [{ token }] = this.context;
+
+      let tracks = [];
+      let data = undefined;
+      let next = undefined;
+      let index = 0;
+
+      let playlistInfo = await getPlaylistInfo(token, playlistToCopy);
+      console.log("Playlist Info", playlistInfo);
+
+      // Create new playlist with the same name
+      let newPlaylist = await createPlaylist(
+        token,
+        currentUser.id,
+        playlistInfo.name,
+        playlistInfo.description
+      );
+      console.log("New playlist", newPlaylist);
+
+      // Get playlist tracks only allow 100 at a time.
+      // The spotify api kindly provides a 'next' property with a url
+      // to get the next 100 tracks.
+      do {
+        data = await getPlaylistTracks(token, playlistToCopy, next);
+        next = data.next;
+        // tracks = tracks.concat(data.items);
+        tracks[index] = data.items;
+        index++;
+      } while (next);
+
+      console.log(tracks);
+
+      // 'Add tracks to playlists' only allows 100 tracks at a time
+      for (let i = 0; i < tracks.length; i++) {
+        let tracklist = tracks[i].map(item => {
+          return item.track.uri;
+        });
+
+        let returnedData = await addTracksToPlaylist(
+          token,
+          newPlaylist.id,
+          tracklist
+        );
+
+        if (returnedData.error) {
+          console.log("Something went wrong!", returnedData);
+          break;
+        }
+        console.log(`Added track set ${i + 1} of ${tracks.length}`);
+      }
+
+      this.setState({
+        isCopying: false,
+        copyCompleted: true,
+        newPlaylistUrl: newPlaylist.external_urls.spotify
+      });
+    }, 1000);
+
+    this.setState({
+      isCopying: true
+    });
   };
 
   onChangePlaylistToCopy = e => {
@@ -136,23 +158,24 @@ export default class Copy extends React.Component {
     this.setState({
       playlistToCopy: "",
       isCopying: false,
-      copyCompleted: false
+      copyCompleted: false,
+      newPlaylistUrl: ""
     });
   };
 
   render = () => {
-    const [{ token }] = this.context;
-
-    if (!token) {
-      return <SomethingWentWrong />;
-    }
-
     const {
+      error,
       currentUser,
       playlistToCopy,
       isCopying,
-      copyCompleted
+      copyCompleted,
+      newPlaylistUrl
     } = this.state;
+
+    if (error) {
+      return <SomethingWentWrong />;
+    }
 
     if (!currentUser || isCopying) {
       return (
@@ -168,8 +191,20 @@ export default class Copy extends React.Component {
           <div className="copy-container">
             <div className="copy-again">
               <h1>Playlist created!</h1>
-              <p>You can find it here.</p>
-              <button className="btn-link">Copy another playlist</button>
+              <p>
+                You can find it{" "}
+                <a
+                  href={newPlaylistUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  here
+                </a>
+                .
+              </p>
+              <button onClick={this.onReset} className="btn-link">
+                Copy another playlist
+              </button>
             </div>
           </div>
         </MainContainer>
