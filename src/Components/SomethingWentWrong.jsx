7218bef1b5a7f8d0cff4a70ee38d45da34ddd6a2
Sam S <salinas144@gmail.com>
2019-08-16 18:02:14 -0400

    Update SomethingWentWrong

--- src/Components/SomethingWentWrong.jsx
+++ src/Components/SomethingWentWrong.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import MainContainer from 'Components/MainContainer'
+import React from "react";
+import MainContainer from "Components/MainContainer";
+
+import "./SomethingWentWrong.css";
 
 export default class SomethingWentWrong extends React.Component {
-    render = () => {
-        return (
-            <MainContainer>
-                <div>Something went wrong. Please <a href="/">go back</a> and try again</div>
-            </MainContainer>
-        )
-    }
+  render = () => {
+    return (
+      <MainContainer>
+        <div className="something-wrong">
+          Something went wrong. Please <a href="/">go back</a> and try again.
+        </div>
+      </MainContainer>
+    );
+  };
 }
