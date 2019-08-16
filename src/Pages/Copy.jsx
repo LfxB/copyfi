88510ea817017f3ad87f94852f44fd0394886358
Sam S <salinas144@gmail.com>
2019-08-16 17:55:26 -0400

    Update Copy page layout and css

--- src/Pages/Copy.jsx
+++ src/Pages/Copy.jsx
@@ -1,156 +1,206 @@
-import React from 'react';
-import { StateContext } from 'State'
-import { setToken } from 'Modules/token'
-import { getHashFragment, hideHashFragment } from 'Helpers/hash'
+import React from "react";
+import { Link } from "react-router-dom";
+import { StateContext } from "State";
+import { setToken } from "Modules/token";
+import { getHashFragment, hideHashFragment } from "Helpers/hash";
 import {
-    getCurrentUserProfile,
-    createPlaylist,
-    getPlaylistInfo,
-    getPlaylistTracks,
-    addTracksToPlaylist
-} from 'Helpers/spotify'
-import MainContainer from 'Components/MainContainer'
-import SomethingWentWrong from 'Components/SomethingWentWrong'
+  getCurrentUserProfile,
+  createPlaylist,
+  getPlaylistInfo,
+  getPlaylistTracks,
+  addTracksToPlaylist
+} from "Helpers/spotify";
+import MainContainer from "Components/MainContainer";
+import SomethingWentWrong from "Components/SomethingWentWrong";
 
-import Spinner from 'Assets/green-spinner-200px.svg'
+import Spinner from "Assets/green-spinner-200px.svg";
 
-const placeholder = "https://open.spotify.com/playlist/37i9dQZEVXcM4tGnSiWue3";
+import "./Copy.css";
+
+const placeholder = "open.spotify.com/playlist/37i9dQZEV";
 
 export default class Copy extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentUser: '',
-            playlistToCopy: '',
-            copyCompleted: false
-        }
+    this.state = {
+      currentUser: "",
+      playlistToCopy: "",
+      isCopying: false,
+      copyCompleted: true
+    };
+  }
+
+  componentDidMount = async () => {
+    const { location } = this.props;
+
+    let hash = getHashFragment(location);
+
+    hideHashFragment();
+
+    if (!hash) return;
+
+    let token = hash.access_token;
+
+    if (token) {
+      // const [ { token }, dispatch ] = this.context;
+      let dispatch = this.context[1];
+
+      let userData = await getCurrentUserProfile(token);
+      // console.log(userData);
+      this.setState({
+        currentUser: userData
+      });
+
+      dispatch(setToken(token));
+    }
+  };
+
+  onFormSubmit = async e => {
+    e.preventDefault();
+
+    let { currentUser, playlistToCopy } = this.state;
+
+    let playlistToCopyArr = playlistToCopy.split("/").filter(Boolean);
+
+    if (playlistToCopyArr.length === 0) {
+      alert(
+        "Your 'Playlist to copy' link isn't in the right format. Should look like this: " +
+          placeholder +
+          ""
+      );
+      return;
     }
 
-    componentDidMount = async () => {
-        const { location } = this.props;
+    playlistToCopy = playlistToCopyArr.pop();
 
-        let hash = getHashFragment(location);
+    const [{ token }] = this.context;
 
-        hideHashFragment();
+    let tracks = [];
+    let data = undefined;
+    let next = undefined;
+    let index = 0;
 
-        if (!hash) return;
+    let playlistInfo = await getPlaylistInfo(token, playlistToCopy);
+    console.log("Playlist Info", playlistInfo);
 
-        let token = hash.access_token;
+    // Create new playlist with the same name
+    let newPlaylist = await createPlaylist(
+      token,
+      currentUser.id,
+      playlistInfo.name,
+      playlistInfo.description
+    );
+    console.log("New playlist", newPlaylist);
 
-        if (token) {
-            // const [ { token }, dispatch ] = this.context;
-            let dispatch = this.context[1];
+    // Get playlist tracks only allow 100 at a time.
+    // The spotify api kindly provides a 'next' property with a url
+    // to get the next 100 tracks.
+    do {
+      data = await getPlaylistTracks(token, playlistToCopy, next);
+      next = data.next;
+      // tracks = tracks.concat(data.items);
+      tracks[index] = data.items;
+      index++;
+    } while (next);
 
-            let userData = await getCurrentUserProfile(token);
-            // console.log(userData);
-            this.setState({
-                currentUser: userData
-            })
+    console.log(tracks);
 
-            dispatch(setToken(token));
-        }
+    // 'Add tracks to playlists' only allows 100 tracks at a time
+    for (let i = 0; i < tracks.length; i++) {
+      let tracklist = tracks[i].map(item => {
+        return item.track.uri;
+      });
+
+      let returnedData = await addTracksToPlaylist(
+        token,
+        newPlaylist.id,
+        tracklist
+      );
+
+      if (returnedData.error) {
+        console.log("Something went wrong!", returnedData);
+        break;
+      }
+      console.log(`Added track set ${i + 1} of ${tracks.length}`);
+    }
+  };
+
+  onChangePlaylistToCopy = e => {
+    this.setState({
+      playlistToCopy: e.target.value
+    });
+  };
+
+  onReset = () => {
+    this.setState({
+      playlistToCopy: "",
+      isCopying: false,
+      copyCompleted: false
+    });
+  };
+
+  render = () => {
+    const [{ token }] = this.context;
+
+    if (!token) {
+      return <SomethingWentWrong />;
     }
 
-    onFormSubmit = async (e) => {
-        e.preventDefault();
+    const {
+      currentUser,
+      playlistToCopy,
+      isCopying,
+      copyCompleted
+    } = this.state;
 
-        let { currentUser, playlistToCopy } = this.state;
-
-        let playlistToCopyArr = playlistToCopy.split("/").filter(Boolean)
-        
-        if (playlistToCopyArr.length === 0) {
-            alert("Your 'Playlist to copy' link isn't in the right format. Should look like this: " + placeholder + "")
-            return;
-        }
-
-        playlistToCopy = playlistToCopyArr.pop();
-
-        const [ { token } ] = this.context;
-
-        let tracks = [];
-        let data = undefined;
-        let next = undefined;
-        let index = 0;
-
-        let playlistInfo = await getPlaylistInfo(token, playlistToCopy);
-        console.log("Playlist Info", playlistInfo);
-
-        // Create new playlist with the same name
-        let newPlaylist = await createPlaylist(token, currentUser.id, playlistInfo.name, playlistInfo.description);
-        console.log("New playlist", newPlaylist);
-
-        // Get playlist tracks only allow 100 at a time.
-        // The spotify api kindly provides a 'next' property with a url
-        // to get the next 100 tracks.
-        do {
-            data = await getPlaylistTracks(token, playlistToCopy, next);
-            next = data.next;
-            // tracks = tracks.concat(data.items);
-            tracks[index] = data.items;
-            index++;
-        } while (next)
-
-        console.log(tracks);
-
-        // 'Add tracks to playlists' only allows 100 tracks at a time
-        for (let i = 0; i < tracks.length; i++) {
-
-            let tracklist = tracks[i].map(item => {
-                return item.track.uri;
-            })
-
-            let returnedData = await addTracksToPlaylist(token, newPlaylist.id, tracklist);
-
-            if (returnedData.error) {
-                console.log("Something went wrong!", returnedData);
-                break;
-            }
-            console.log(`Added track set ${i + 1} of ${tracks.length}`)
-        }
+    if (!currentUser || isCopying) {
+      return (
+        <MainContainer>
+          <img className="green-spinner" src={Spinner} alt={"Loading..."} />
+        </MainContainer>
+      );
     }
 
-    onChangePlaylistToCopy = (e) => {
-        this.setState({
-            playlistToCopy: e.target.value
-        })
+    if (copyCompleted) {
+      return (
+        <MainContainer>
+          <div className="copy-container">
+            <div className="copy-again">
+              <h1>Playlist created!</h1>
+              <p>You can find it here.</p>
+              <button className="btn-link">Copy another playlist</button>
+            </div>
+          </div>
+        </MainContainer>
+      );
     }
 
-    render = () => {
-        const [ { token } ] = this.context;
-
-        if (!token) {
-            return <SomethingWentWrong />
-        }
-
-        const { currentUser, playlistToCopy, copyCompleted } = this.state;
-
-        if (!currentUser) {
-            return <MainContainer>
-                <img className="green-spinner" src={Spinner} alt={"Loading..."} />
-            </MainContainer>
-        }
-
-        if (copyCompleted) {
-            return <MainContainer>
-                <div className="copy-again">
-                    
-                </div>
-            </MainContainer>
-        }
-
-        return (
-            <MainContainer>
-                <div className="copy-container">
-                    <h1>{`Welcome, ${currentUser.id}`}</h1>
-                    <form onSubmit={this.onFormSubmit}>
-                        <h1>Playlist to copy</h1>
-                        <input required type="text" onChange={this.onChangePlaylistToCopy} value={playlistToCopy} placeholder={placeholder}/>
-                        <input type="submit" value="Copy!" />
-                    </form>
-                </div>
-            </MainContainer>
-        )
-    }
+    return (
+      <MainContainer>
+        <div className="copy-container">
+          <div className="copy-centered">
+            <h1>{`Welcome, ${currentUser.id}`}</h1>
+            <form className="copy-form" onSubmit={this.onFormSubmit}>
+              <h3>Playlist link</h3>
+              <input
+                required
+                className="copy-form-link-input"
+                type="text"
+                onChange={this.onChangePlaylistToCopy}
+                value={playlistToCopy}
+                placeholder={placeholder}
+              />
+              <input
+                className="copy-form-submit btn-link"
+                type="submit"
+                value="Copy!"
+              />
+            </form>
+          </div>
+        </div>
+      </MainContainer>
+    );
+  };
 }
 Copy.contextType = StateContext;
