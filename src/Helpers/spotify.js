01a0c1fd2a29f802cfc45b554e93c20d10587cb6
Sam S <salinas144@gmail.com>
2019-08-16 19:26:28 -0400

    Update redirect uri

--- src/Helpers/spotify.js
+++ src/Helpers/spotify.js
@@ -1,8 +1,8 @@
-const authEndpoint = 'https://accounts.spotify.com/authorize';
+const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "cfa196a5dac044e48e7b749920224934";
 
-const redirectUri = "http://localhost:3000/copy";
+const redirectUri = "https://copyfi.netlify.com//copy";
 
 const scopes = [
   "playlist-modify-public",
@@ -11,29 +11,39 @@ const scopes = [
   "user-read-private"
 ];
 
-const makeid = (length) => {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const makeid = length => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
+};
 
-export const authLink =  authEndpoint + '?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&scope=' + scopes.join("%20") + '&response_type=token&show_dialog=true&state=' + makeid(16);
+export const authLink =
+  authEndpoint +
+  "?client_id=" +
+  clientId +
+  "&redirect_uri=" +
+  redirectUri +
+  "&scope=" +
+  scopes.join("%20") +
+  "&response_type=token&show_dialog=true&state=" +
+  makeid(16);
 
-export const getCurrentUserProfile = async (token) => {
+export const getCurrentUserProfile = async token => {
   let response = await fetch(`https://api.spotify.com/v1/me`, {
-    method: 'GET',
+    method: "GET",
     headers: new Headers({
-    Accept: "application/json",
-    Authorization: `Bearer ${token}`
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
     })
   });
-  let data = await response.json()
+  let data = await response.json();
   return data;
-}
+};
 
 // export const getCurrentUserPlaylists = async (token) => {
 //   let response = await fetch(`https://api.spotify.com/v1/me`, {
@@ -48,59 +58,67 @@ export const getCurrentUserProfile = async (token) => {
 // }
 
 export const createPlaylist = async (token, userId, name, description) => {
-  let response = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-    method: 'POST',
-    headers: new Headers({
-    Accept: "application/json",
-    Authorization: `Bearer ${token}`
-    }),
-    body: JSON.stringify({
-      name,
-      description,
-      public: true
-    })
-  });
-  let data = await response.json()
+  let response = await fetch(
+    `https://api.spotify.com/v1/users/${userId}/playlists`,
+    {
+      method: "POST",
+      headers: new Headers({
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`
+      }),
+      body: JSON.stringify({
+        name,
+        description,
+        public: true
+      })
+    }
+  );
+  let data = await response.json();
   return data;
-}
+};
 
 export const getPlaylistInfo = async (token, playlistId) => {
   let url = `https://api.spotify.com/v1/playlists/${playlistId}?fields=fields%3Dhref%2Cname%2C%2Cdescription`;
   let response = await fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: new Headers({
-    Accept: "application/json",
-    Authorization: `Bearer ${token}`
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
     })
   });
-  let data = await response.json()
+  let data = await response.json();
   return data;
-}
+};
 
 export const getPlaylistTracks = async (token, playlistId, next) => {
-  let url = next ? next : `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+  let url = next
+    ? next
+    : `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
   let response = await fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: new Headers({
-    Accept: "application/json",
-    Authorization: `Bearer ${token}`
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
     })
   });
-  let data = await response.json()
+  let data = await response.json();
   return data;
-}
+};
 
 export const addTracksToPlaylist = async (token, playlistId, tracklist) => {
-  let response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-    method: 'POST',
-    headers: new Headers({
-    Accept: "application/json",
-    Authorization: `Bearer ${token}`
-    }),
-    body: JSON.stringify({
-      uris: tracklist
-    })
-  });
-  let data = await response.json()
+  let response = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+    {
+      method: "POST",
+      headers: new Headers({
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`
+      }),
+      body: JSON.stringify({
+        uris: tracklist
+      })
+    }
+  );
+  let data = await response.json();
   return data;
-}
+};
